129a825b8b22e163102fa4fd633cb2f55f3ef681
honeybadger2788 <noe.carosella@gmai.com>
2024-01-23 17:13:21 -0300

    add create task controller

--- api-task/src/main/java/com/noeliacarosella/apitask/controller/TaskController.java
+++ api-task/src/main/java/com/noeliacarosella/apitask/controller/TaskController.java
@@ -1,9 +1,22 @@
 package com.noeliacarosella.apitask.controller;
 
+import com.noeliacarosella.apitask.model.Task;
+import com.noeliacarosella.apitask.service.ITaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("tasks")
 public class TaskController {
+
+    @Autowired
+    ITaskService taskService;
+    @PostMapping("/add")
+    public ResponseEntity<Task> postTask(@RequestBody Task task) {
+        return ResponseEntity.ok(taskService.create(task));
+    }
 }
--- api-task/src/main/java/com/noeliacarosella/apitask/service/impl/TaskService.java
+++ api-task/src/main/java/com/noeliacarosella/apitask/service/impl/TaskService.java
@@ -1,11 +1,17 @@
 package com.noeliacarosella.apitask.service.impl;
 
 import com.noeliacarosella.apitask.model.Task;
+import com.noeliacarosella.apitask.repository.TaskRepository;
 import com.noeliacarosella.apitask.service.ITaskService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class TaskService implements ITaskService {
+
+    @Autowired
+    TaskRepository taskRepository;
+
     @Override
     public Task create(Task task) {
-        return null;
+        return taskRepository.save(task);
     }
 }
