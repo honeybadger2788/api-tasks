61adec4b7ed368ea9ec55560664ff47762cbe1a3
honeybadger2788 <noe.carosella@gmai.com>
2024-01-23 17:05:21 -0300

    add task service

---
+++ api-task/src/main/java/com/noeliacarosella/apitask/service/ITaskService.java
@@ -0,0 +1,7 @@
+package com.noeliacarosella.apitask.service;
+
+import com.noeliacarosella.apitask.model.Task;
+
+public interface ITaskService {
+    public Task create(Task task);
+}
---
+++ api-task/src/main/java/com/noeliacarosella/apitask/service/impl/TaskService.java
@@ -0,0 +1,11 @@
+package com.noeliacarosella.apitask.service.impl;
+
+import com.noeliacarosella.apitask.model.Task;
+import com.noeliacarosella.apitask.service.ITaskService;
+
+public class TaskService implements ITaskService {
+    @Override
+    public Task create(Task task) {
+        return null;
+    }
+}
