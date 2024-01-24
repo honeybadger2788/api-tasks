86294da37373267ad21d5ab5e84a162a11d2c4aa
honeybadger2788 <noe.carosella@gmai.com>
2024-01-24 15:17:09 -0300

    create and add ErrorResponse

--- api-task/src/main/java/com/noeliacarosella/apitask/exceptions/GlobalExceptionHandler.java
+++ api-task/src/main/java/com/noeliacarosella/apitask/exceptions/GlobalExceptionHandler.java
@@ -1,24 +1,44 @@
 package com.noeliacarosella.apitask.exceptions;
 
+import com.noeliacarosella.apitask.model.ErrorResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
-    public ResponseEntity allError(Exception ex, WebRequest req) {
-        return new ResponseEntity(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<ErrorResponse> allError(Exception ex, WebRequest req) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                ex.getMessage(),
+                req.getDescription(false)
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity notFoundError(ResourceNotFoundException ex){
-        return new ResponseEntity(ex.getMessage(),HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> notFoundError(ResourceNotFoundException ex, WebRequest req){
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                HttpStatus.NOT_FOUND.getReasonPhrase(),
+                ex.getMessage(),
+                req.getDescription(false)
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
     }
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity badRequest(BadRequestException ex){
-        return new ResponseEntity(ex.getMessage(),HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> badRequest(BadRequestException ex, WebRequest req){
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                ex.getMessage(),
+                req.getDescription(false)
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
---
+++ api-task/src/main/java/com/noeliacarosella/apitask/model/ErrorResponse.java
@@ -0,0 +1,6 @@
+package com.noeliacarosella.apitask.model;
+
+public class ErrorResponse {
+    public ErrorResponse(int value, String reasonPhrase, String message, String description) {
+    }
+}
