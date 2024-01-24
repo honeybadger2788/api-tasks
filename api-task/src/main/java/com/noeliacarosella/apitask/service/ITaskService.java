3746f1796a8d57dd7e45fc07b55cc1a60d4dfffb
Noelia Carosella <72532638+honeybadger2788@users.noreply.github.com>
2024-01-24 12:27:17 -0300

    Feature/list tasks (#3)
    * add list task service
    
    * add list task controller
    
    ---------
    
    Co-authored-by: honeybadger2788 <noe.carosella@gmai.com>

--- api-task/src/main/java/com/noeliacarosella/apitask/service/ITaskService.java
+++ api-task/src/main/java/com/noeliacarosella/apitask/service/ITaskService.java
@@ -2,6 +2,9 @@ package com.noeliacarosella.apitask.service;
 
 import com.noeliacarosella.apitask.model.Task;
 
+import java.util.List;
+
 public interface ITaskService {
     public Task create(Task task);
+    public List<Task> listTasks();
 }
