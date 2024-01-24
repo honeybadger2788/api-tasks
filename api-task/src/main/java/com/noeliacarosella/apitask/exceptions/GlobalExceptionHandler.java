7eac1e67f42ab1ffbae227916e3565006dd0dd8c
honeybadger2788 <noe.carosella@gmai.com>
2024-01-24 14:47:26 -0300

    create and add global error handler

---
+++ api-task/src/main/java/com/noeliacarosella/apitask/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.noeliacarosella.apitask.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity allError(Exception ex, WebRequest req) {
+        return new ResponseEntity(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
