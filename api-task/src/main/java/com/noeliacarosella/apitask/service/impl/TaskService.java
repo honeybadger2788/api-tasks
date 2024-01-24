176c7b230f8564eee0c603d89c664ded215130e3
honeybadger2788 <noe.carosella@gmai.com>
2024-01-24 12:23:25 -0300

    add list task service

--- api-task/src/main/java/com/noeliacarosella/apitask/service/impl/TaskService.java
+++ api-task/src/main/java/com/noeliacarosella/apitask/service/impl/TaskService.java
@@ -5,6 +5,8 @@ import com.noeliacarosella.apitask.repository.TaskRepository;
 import com.noeliacarosella.apitask.service.ITaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
 public class TaskService implements ITaskService {
 
     @Autowired
@@ -13,5 +15,10 @@ public class TaskService implements ITaskService {
     @Override
     public Task create(Task task) {
         return taskRepository.save(task);
+    }
+
+    @Override
+    public List<Task> listTasks() {
+        return taskRepository.findAll();
     }
 }
