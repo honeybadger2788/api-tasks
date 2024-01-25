01b22fe27030f720eaf5a636568ef7c3b6cefc3b
honeybadger2788 <noe.carosella@gmai.com>
2024-01-25 18:49:02 -0300

    add dummy data for test

--- api-task/src/test/java/com/noeliacarosella/apitask/service/impl/TaskServiceTest.java
+++ api-task/src/test/java/com/noeliacarosella/apitask/service/impl/TaskServiceTest.java
@@ -1,8 +1,30 @@
 package com.noeliacarosella.apitask.service.impl;
 
+import com.noeliacarosella.apitask.model.Task;
+import com.noeliacarosella.apitask.repository.TaskRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class TaskServiceTest {
+    @Mock
+    TaskRepository taskRepository;
 
+    @InjectMocks
+    TaskService taskService;
+
+    private List<Task> dummyTasks;
+
+    @BeforeEach
+    public void setUp() {
+        dummyTasks = new ArrayList<>();
+        dummyTasks.add(new Task("Test Task 1", "Description 1"));
+        dummyTasks.add(new Task("Test Task 2", "Description 2"));
+        dummyTasks.add(new Task("Test Task 3", "Description 3"));
+    }
 }
